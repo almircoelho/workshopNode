7cdce004570c0efd9f6b293abc4fc9805fdd7559
Almir Coelho <almircoelho.qi@gmail.com>
2017-01-24 21:34:05 -0200

    Integração com banco adicionada

--- index.js
+++ index.js
@@ -1,33 +1,71 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const middleware = require('./log-middleware')
+const middleware = require('./log-middleware');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Desenvolvedores = require('./models/desenvolvedores');
+
+var options = {
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
+
+mongoose.connect('mongodb://teste:teste123@ds133438.mlab.com:33438/workshop', options, (err) => {
+    if (err) console.log('Não consegui conectar');
+    else console.log('Conectado com sucesso!');
+});
+
+app.set('view engine', 'pug');
 
 app.use(middleware);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    fs.readFile('./views/index.html', 'UTF-8', (err, content) => {
-        if (err) res.status(500).send('Ocorreu um erro');
-
-        res.status(200).send(content);
-    });
+    res.render('index', { nomes: ['teste 1', 'teste 2', 'teste 3'] });
 });
 
 app.get('/contato', (req, res) => {
-    fs.readFile('./views/contato.html', 'UTF-8', (err, content) => {
-        if (err) res.status(500).send('Ocorreu um erro');
-
-        res.send(content);
-    });
+    res.render('contato');
 });
 
 app.get('/sobre', (req, res) => {
-    fs.readFile('./views/sobre.html', 'UTF-8', (err, content) => {
-        if (err) res.status(500).send('Ocorreu um erro');
-
-        res.send(content);
-    });
+    res.render('sobre');
 });
+
+app.get('/cadastrar', (req, res) => {
+    res.render('cadastrar');
+});
+
+var indice = 0;
+app.route('/desenvolvedores')
+    .post((req, res) => {
+        var desenvolvedores = new Desenvolvedores({
+            nome: req.body.nome,
+            email: req.body.email,
+            indice: ++indice
+        });
+        desenvolvedores.save((err) => {
+            if (err) res.status(500).send('Ocorreu um erro');
+
+            Desenvolvedores.find({},
+                (err, desenvolvedores) => {
+                    if (err) res, status(500).send('Deu erro');
+
+                    res.render('desenvolvedores', { desenvolvedores: desenvolvedores });
+                });
+        });
+    })
+    .get((req, res) => {
+        Desenvolvedores.find({},
+            (err, desenvolvedores) => {
+                if (err) res, status(500).send('Deu erro');
+
+                res.render('desenvolvedores', { desenvolvedores: desenvolvedores });
+            });
+    });
+
 
 app.listen(3001, () => {
     console.log('Você está rodando a aplicação na porta 3001');
